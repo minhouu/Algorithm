95963e19fccc7671b6c78be7f2b5f957c55a3453
minhouu <minhouu>
2023-08-10 17:50:47 +0900

    0810 공부기록

---
+++ 바킹독/0x05-스택/src/BOJ2493/Main.java
@@ -0,0 +1,31 @@
+package BOJ2493;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine()); // 탑의 개수
+		StringTokenizer st = new StringTokenizer(bf.readLine()); // 탑들 전부 문자열로 받아옴
+		ArrayDeque<int[]> stack = new ArrayDeque<>();
+		stack.push(new int[] { (int) (1e8 + 1), 0 }); // 최대값 이상의 초기값 stack에 넣음 
+		// 자기보다 이전에 높이가 높은 탑이 없으면(이것과 비교할때는 반드시 작기 때문에), 0의 값을 가지게 해줌
+		StringBuilder output = new StringBuilder();
+		
+		
+		for (int i = 0; i < N; i++) {
+			int h = Integer.parseInt(st.nextToken()); // 높이 입력받음
+			while (stack.peek()[0] < h) {
+				stack.pop();
+			}
+			output.append(stack.peek()[1]);
+			output.append(" ");
+			stack.push(new int[] {h, i+1});
+		}
+		System.out.println(output);
+	}
+}
